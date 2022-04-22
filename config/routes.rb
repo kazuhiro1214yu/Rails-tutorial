51d2307c8dd031017a758460e11f88f7a390103d
kazuhirotakahashi <kazuhiro1214yu@gmail.com>
2022-04-22 01:50:00 +0000

    3.3.2

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@ Rails.application.routes.draw do
   get 'top',to: 'home#top'
   get 'about', to: 'home#about'
   get 'posts/index', to: 'posts#index'
+  get 'posts/:id', to: 'posts#show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
