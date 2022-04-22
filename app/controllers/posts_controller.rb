e60c0bb481955cb7bdbc4682594f51e201a20cae
kazuhirotakahashi <kazuhiro1214yu@gmail.com>
2022-04-22 02:26:11 +0000

    3.7.3

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,8 +1,11 @@
 class PostsController < ApplicationController
+
+  def new
+  end 
+
   def index
     @posts = Post.all
   end
-  
   def show
     @post = Post.find(params[:id])
   end 
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,8 @@ Rails.application.routes.draw do
   root 'home#top'
   get 'top',to: 'home#top'
   get 'about', to: 'home#about'
+  get 'posts/new', to: 'posts#new'
   get 'posts/index', to: 'posts#index'
   get 'posts/:id', to: 'posts#show', as: :post
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
