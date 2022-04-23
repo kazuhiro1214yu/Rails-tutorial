ae72f49b27e41f099c3e52fabed189322379b8c6
kazuhirotakahashi <kazuhiro1214yu@gmail.com>
2022-04-23 12:10:08 +0000

    7.1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20220422115601) do
+ActiveRecord::Schema.define(version: 20220423120740) do
 
   create_table "posts", force: :cascade do |t|
     t.text "content"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20220422115601) do
     t.string "email"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "image"
   end
 
 end
