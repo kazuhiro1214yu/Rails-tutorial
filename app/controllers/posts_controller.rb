b0175aeebf5c300375294d3e6c28d13ffd3d0a06
kazuhirotakahashi <kazuhiro1214yu@gmail.com>
2022-04-26 04:45:56 +0000

    9.8.1_before

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,6 @@
 class PostsController < ApplicationController
   before_action :authenticate_user
+  before_action :limitation_correct_user, only: [:edit, :update, :destroy]
   
 
   def new
@@ -48,6 +49,14 @@ class PostsController < ApplicationController
     @post.destroy
     flash[:notice] = "投稿を削除しました｡"
     redirect_to posts_index_url
+  end 
+  
+  def limitation_correct_user
+    @post = Post.find(params[:id])
+    unless @post.user_id == @current_user.id
+      flash[:notice] = "自分意外のユーザーの投稿は編集できません｡"
+      redirect_to posts_index_url
+    end
   end 
   
 end
